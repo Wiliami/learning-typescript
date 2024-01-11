f073c72f6cfe3184f619861d415322441832f1d2
wiliami <wiliaup97@gmail.com>
2024-01-10 21:55:05 -0400

    chore: add files build typescript to javascript

---
+++ dist/teste.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// src/teste.ts
+function saveUserToDatabase(user) {
+  console.log(user);
+}
+saveUserToDatabase({
+  name: "Wiliamis",
+  age: 26
+});
---
+++ src/teste.ts
@@ -0,0 +1,34 @@
+interface User {
+    name: string
+    age: number
+};
+
+interface CreateUser {
+    name: string
+    email: string
+    password: string
+    telephone: number
+}
+
+function saveUserToDatabase(user: User) {
+    console.log(user);
+};
+
+
+saveUserToDatabase({
+    name: 'Wiliamis',
+    age: 26
+});
+
+
+function createUser(dataUser: CreateUser) {
+    console.log(dataUser);
+};
+
+
+createUser({
+    name: 'Wiliamis',
+    email: 'oliveirawiliamis34@gmail.com',
+    password: '12345678',
+    telephone: 99999999
+});
