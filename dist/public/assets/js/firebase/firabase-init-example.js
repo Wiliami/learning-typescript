7b0539f2b6a37cf4b33ae6b63d5644ffeb8e8188
wiliami <wiliaup97@gmail.com>
2024-01-30 10:23:15 -0400

    add firebase

---
+++ dist/public/assets/js/firebase/firabase-init-example.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+/* eslint-disable indent */
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
